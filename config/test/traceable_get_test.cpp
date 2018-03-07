a0247f73624fd5f481724edc8a31647aa2718f7f
Anna <gaoyuanhezhihao@139.com>
2018-03-07 22:03:13 +0800

    config: added traceable get param method

---
+++ config/test/traceable_get_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Config.hpp"
+
+int main(int argc, const char ** argv) {
+    configs.init_by_string("a=1\n");
+    cout << string(get_param("c")) << endl;
+    //cout << string(configs["c"]) << endl;
+}
